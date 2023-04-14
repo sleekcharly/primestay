6df96384b8f23ff3fa58360567641bb125a6801d
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2023-04-14 12:12:06 +0100

    Created SafeUser type to augment errors from currentUser

---
+++ app/types/index.ts
@@ -0,0 +1,12 @@
+// type definition to solve type error on currentUser object
+
+import { User } from '@prisma/client';
+
+export type SafeUser = Omit<
+  User,
+  'createdAt' | 'updatedAt' | 'emailVerified'
+> & {
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
+};
