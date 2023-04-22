a0a3bac0631842d17b9815d0ae84a85b8881abd1
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2023-04-22 13:11:06 +0100

    created listings on page.tsx and listing component for fetching listings using serverside components

---
+++ app/components/listings/ListingCard.tsx
@@ -0,0 +1,126 @@
+'use client';
+
+import useCountries from '@/app/hooks/useCountries';
+import { SafeUser } from '@/app/types';
+import { Listing, Reservation } from '@prisma/client';
+import { format } from 'date-fns';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React, { useCallback, useMemo } from 'react';
+import HeartButton from '../HeartButton';
+import Button from '../Button';
+
+interface ListingCardProps {
+  data: Listing;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId?: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  reservation,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId = '',
+  currentUser,
+}) => {
+  // get router
+  const router = useRouter();
+
+  // use countries
+  const { getByValue } = useCountries();
+
+  const location = getByValue(data.locationValue);
+
+  //   handle cancel function
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (disabled) {
+        return;
+      }
+
+      onAction?.(actionId);
+    },
+    [onAction, actionId, disabled],
+  );
+
+  const price = useMemo(() => {
+    if (reservation) {
+      return reservation.totalPrice;
+    }
+
+    return data.price;
+  }, [reservation, data.price]);
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+
+    // start date
+    const start = new Date(reservation.startDate);
+
+    // end date
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+  }, [reservation]);
+
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="col-span-1 cursor-pointer group"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div
+          className="
+        aspect-square
+        w-full relative overflow-hidden rounded-xl
+    "
+        >
+          <Image
+            fill
+            alt="listing"
+            src={data.imageSrc}
+            className="object-cover h-full w-full group-hover:scale-110 transition"
+          />
+
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+
+        <div className="font-semibold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+
+        <div className="font-white text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semi-bold">${price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ListingCard;
