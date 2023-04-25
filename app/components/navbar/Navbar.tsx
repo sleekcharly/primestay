fc3316c8ca5dc2a1e8bea4136cb024ff0a9ce7c0
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2023-04-25 17:04:56 +0100

    created the listing reservatio component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -15,7 +15,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
-    <div className="fixed w-full bg-white shadow-sm">
+    <div className="fixed w-full bg-white shadow-sm z-10">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
